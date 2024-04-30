02c76ae2e820f64856936e0219349effdb244094
sumanthakur93 <132763109+sumanthakur93@users.noreply.github.com>
2024-04-30 22:11:47 +0530

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,7 +12,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export const registerApi = (payload: RegisterSchemaType) =>

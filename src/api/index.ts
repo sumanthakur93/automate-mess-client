85d81b4df655aae907442d558bcff83006d7e29f
sumanthakur93 <132763109+sumanthakur93@users.noreply.github.com>
2024-04-30 22:20:52 +0530

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,8 +11,10 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
+    'Access-Control-Allow-Origin': '*',
   },
-  // withCredentials: true,
+  withCredentials: true,
+  credentials: 'include'
 });
 
 export const registerApi = (payload: RegisterSchemaType) =>
